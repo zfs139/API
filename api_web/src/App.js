c1063590e7eabc85d26c738854b2ea3d8585aa4a
zhang_fengshuo <zfs@bjtu.edu.cn>
2021-10-30 14:40:27 +0800

    first commit

---
+++ api_web/src/App.js
@@ -0,0 +1,15 @@
+import {BrowserRouter,Route,Link} from 'react-router-dom'
+import {Button} from 'antd'
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+      <Button>
+      首页
+      </Button>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
